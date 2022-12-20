f07fa8fbe33a7e3fcf3b888dc4542a210882e9c3
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-20 08:08:08 -0300

    class 3.8

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
-let gameSpeed = 20;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "layer-1.png";
@@ -14,6 +14,14 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = "layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "layer-5.png";
+
+const slider = document.getElementById("slider");
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById("showGameSpeed");
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener("change", function (e) {
+  gameSpeed = e.target.value;
+});
 
 class Layer {
   constructor(image, speedModifier) {
